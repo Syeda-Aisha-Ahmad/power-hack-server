e7134314f70aae1c82140811c61d72562a4c8bc8
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2023-01-29 19:51:35 +0600

    add billing delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,14 @@ async function run() {
             const query = {};
             const result = await addBillingCollection.find(query).sort({ $natural: -1 }).toArray();
             res.send(result)
+        })
+
+        // delete billing data
+        app.delete('/delete-billing/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await addBillingCollection.deleteOne(query);
+            res.send(result);
         })
 
     }
